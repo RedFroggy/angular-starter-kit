bfdd613a6db7fc377660054c728d12311fc37d25
Michael Desigaud <michael.desigaud@redfroggy.fr>
2019-11-16 18:58:39 +0100

    feat: #9: Add release workflow

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  platform: 'gitlab',
+  endpoint: 'https://gitlab.com/api/v4',
+  token: process.env.GITLAB_TOKEN,
+  repositories: ['red-froggy/angular-cli'],
+  logLevel: 'info',
+  requireConfig: true,
+  onboarding: true,
+  onboardingConfig: {
+    extends: ['config:base'],
+    prConcurrentLimit: 5
+  },
+  enabledManagers: ['npm']
+};
