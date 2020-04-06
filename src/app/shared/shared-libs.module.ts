2762276f96a9b1cb08594ee47528bf7dccaeb18b
Michael Desigaud <michael.desigaud@redfroggy.fr>
2020-04-06 14:09:04 +0200

    chore: change prettier options

--- src/app/shared/shared-libs.module.ts
+++ src/app/shared/shared-libs.module.ts
@@ -4,8 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  exports: [HttpClientModule, FormsModule, CommonModule, RouterModule, TranslateModule],
-}) /** * This module is used to import dans
-configure vendors libraries */
-export class SharedLibsModule {}
+  exports: [
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    RouterModule,
+    TranslateModule
+  ]
+})
+/**
+ * This module is used to import dans configure vendors libraries
+ */
+export class SharedLibsModule {
+}
