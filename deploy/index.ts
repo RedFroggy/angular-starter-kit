6991d6041f75f6e9d8f5ce9d3d9b5bb1a7e07b01
djay <david.jay@redfroggy.fr>
2020-05-11 09:32:40 +0200

    ci(aws):  add aws deploy

---
+++ deploy/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import cdk = require('@aws-cdk/core');
+import { StaticSite } from './static-site';
+
+/**
+ * This stack relies on getting the domain name from CDK context.
+ * Use 'cdk synth -c domain=mystaticsite.com -c subdomain=www -c certificateArn=mycert'
+ * Or add the following to cdk.json:
+ * {
+ *   "context": {
+ *     "domain": "mystaticsite.com",
+ *     "subdomain": "www",
+ *     "certificateArn": "mycert"
+ *   }
+ * }
+**/
+class AngularStaticSiteStack extends cdk.Stack {
+    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+        super(parent, name, props);
+
+        new StaticSite(this, 'StaticSite', {
+          domainName: this.node.tryGetContext('domain'),
+          siteSubDomain: this.node.tryGetContext('subdomain'),
+          certificateArn: this.node.tryGetContext('certificateArn'),
+        });
+   }
+}
+
+const app = new cdk.App();
+
+new AngularStaticSiteStack(app, 'MyStaticSite', { env: {
+    // Stack must be in us-east-1, because the ACM certificate for a
+    // global CloudFront distribution must be requested in us-east-1.
+    region: 'us-east-1'
+}});
+
+app.synth();
