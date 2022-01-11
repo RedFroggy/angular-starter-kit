071db8ca8e1e917ff1b621eaa8b4e6d31497477d
Michael Desigaud <michael.desigaud@redfroggy.fr>
2022-01-11 17:22:26 +0100

    chore: fix build

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,10 @@
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-@NgModule({})
+@NgModule({
+  imports: [HttpClientModule],
+})
 export class CoreModule {
   /**
    * Declare only services that need to be singleton
