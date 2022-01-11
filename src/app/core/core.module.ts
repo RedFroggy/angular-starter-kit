4fc19066d60a6d3caa5cdecf7a8d3108ec5c49a2
Michael Desigaud <michael.desigaud@redfroggy.fr>
2022-01-12 00:10:03 +0100

    chore: fix core module typo

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,8 @@
-import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [HttpClientModule],
+  providers: [], // Declare singleton services here
 })
-export class CoreModule {
-  /**
-   * Declare only services that need to be singleton
-   * Only one instance of these services will be shared among lazy loaded modules
-   */
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: CoreModule,
-    };
-  }
-}
+export class CoreModule {}
