b6e5a0968058a9d36703c703716c32f4e393a74a
Michael Desigaud <michael.desigaud@redfroggy.fr>
2022-01-12 08:47:26 +0100

    chore: fix build errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppConfigModule } from './app-config.module';
 import { FormsModule } from '@angular/forms';
-import { ApiModule } from 'app/shared/api';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, AppConfigModule, AppRoutingModule, ApiModule],
+  imports: [BrowserModule, FormsModule, AppConfigModule, AppRoutingModule, HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [HttpClientModule],
+  imports: [],
   providers: [], // Declare singleton services here
 })
 export class CoreModule {}
